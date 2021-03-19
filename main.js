802ad2e005edc394a380479ce985d30ceef612f8
ebdevStudent <78286626+ebdevStudent@users.noreply.github.com>
2021-03-18 21:38:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function btn1(){
+document.getElementById("button1").innerHTML="press me!";
+}
+function ipt1(){
+    document.getElementById("input1").placeholder="Enter";
+}
+function changeimg1(){
+document.getElementById("img1").src="rainbowatom.png"
+}
